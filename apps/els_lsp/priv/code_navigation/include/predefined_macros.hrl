fcde967dc1ef22dbf56bd303eca11ee5d1330db1
Benjamin Krenn <benjamin.krenn@leftshift.one>
2024-04-15 22:20:19 +0200

    fix: define macros only on supported otp versions

--- apps/els_lsp/priv/code_navigation/include/predefined_macros.hrl
+++ apps/els_lsp/priv/code_navigation/include/predefined_macros.hrl
@@ -1,15 +1,3 @@
-% -export([
-%          mod/0,
-%          mod_str/0,
-%          file/0,
-%          line/0,
-%          machine/0,
-%          fun_name/0,
-%          arity/0,
-%          release/0,
-%          favail/1
-%         ]).
-
 mod() -> ?MODULE.
 mod_str() -> ?MODULE_STRING.
 file() -> ?FILE.
@@ -17,5 +5,9 @@ line() -> ?LINE.
 machine() -> ?MACHINE.
 fun_name() -> ?FUNCTION_NAME.
 arity() -> ?FUNCTION_ARITY.
-release() -> ?OTP_RELEASE.
-favail(X) -> ?FEATURE_AVAILABLE(X).
+-ifdef(OTP_RELEASE).
+    release() -> ?OTP_RELEASE.
+    -if(OTP_RELEASE >= 25).
+        favail(X) -> ?FEATURE_AVAILABLE(X).
+    -endif.
+-endif.
